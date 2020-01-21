0b0e7422abd979f6c1b82169833d2b7e4424a975
Peter Himschoot <peter@u2u.be>
2020-01-21 17:00:43 +0100

    C4

--- Chapter4/PizzaPlace/PizzaPlace/Client/Startup.cs
+++ Chapter4/PizzaPlace/PizzaPlace/Client/Startup.cs
@@ -1,17 +1,18 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using PizzaPlace.Shared;
 
 namespace PizzaPlace.Client
 {
-    public class Startup
+  public class Startup
+  {
+    public void ConfigureServices(IServiceCollection services)
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        public void Configure(IComponentsApplicationBuilder app)
-        {
-            app.AddComponent<App>("app");
-        }
+      services.AddTransient<IMenuService, HardCodedMenuService>();
+      services.AddTransient<IOrderService, ConsoleOrderService>();
     }
+
+    public void Configure(IComponentsApplicationBuilder app) 
+      => app.AddComponent<App>("app");
+  }
 }
