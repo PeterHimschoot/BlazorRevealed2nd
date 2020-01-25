f5f177e777383ddc9f3bfe54bc0fec9ee0801858
Peter Himschoot <peter@u2u.be>
2020-01-25 15:37:11 +0100

    Add Disposable interface

--- Chapter4/BlazorLifetime/BlazorLifetime/Shared/SingletonService.cs
+++ Chapter4/BlazorLifetime/BlazorLifetime/Shared/SingletonService.cs
@@ -2,11 +2,14 @@
 
 namespace BlazorLifetime.Shared
 {
-  public class SingletonService
+  public class SingletonService : IDisposable
   {
     public Guid Guid { get; set; }
 
     public SingletonService()
       => Guid = Guid.NewGuid();
+
+    public virtual void Dispose() 
+      => Console.WriteLine($"{nameof(SingletonService)} disposed.");
   }
 }
--- Chapter4/BlazorLifetime/BlazorLifetime/Shared/TransientService.cs
+++ Chapter4/BlazorLifetime/BlazorLifetime/Shared/TransientService.cs
@@ -2,12 +2,16 @@
 
 namespace BlazorLifetime.Shared
 {
-  public class TransientService
+  public class TransientService : IDisposable
   {
     public Guid Guid { get; set; }
 
     public TransientService()
       => Guid = Guid.NewGuid();
+
+    public virtual void Dispose() 
+      => Console.WriteLine($"{nameof(TransientService)} disposed.");
+
   }
 
 }
