da4fbb3ce0aac0451bd0e00907921aaa1fc4cad3
Peter Himschoot <peter@u2u.be>
2020-02-01 17:24:41 +0100

    Update to 3.2 Preview 1

--- Chapter6/MyFirstBlazor/MyFirstBlazor/Client/Program.cs
+++ Chapter6/MyFirstBlazor/MyFirstBlazor/Client/Program.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MyFirstBlazor.Client.Services;
+using MyFirstBlazor.Shared;
+using System.Threading.Tasks;
 
 namespace MyFirstBlazor.Client
 {
-    public class Program
+  public class Program
+  {
+    public static async Task Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+      builder.Services.AddSingleton<IProductsService, HardCodedProductService>();
+      builder.RootComponents.Add<App>("app");
+
+      await builder.Build().RunAsync();
     }
+  }
 }
