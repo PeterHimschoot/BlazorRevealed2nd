9c039552d828be32d16e60242b81f5f83dc994a3
Peter Himschoot <peter@u2u.be>
2020-02-02 17:33:40 +0100

    Ch8

---
+++ Chapter8/PizzaPlace/PizzaPlace/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using PizzaPlace.Client.Services;
+using PizzaPlace.Shared;
+using System.Threading.Tasks;
+
+namespace PizzaPlace.Client
+{
+  public class Program
+  {
+    public static async Task Main(string[] args)
+    {
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
+      builder.RootComponents.Add<App>("app");
+      builder.Services.AddTransient<IMenuService, MenuService>();
+      builder.Services.AddTransient<IOrderService, OrderService>();
+      builder.Services.AddSingleton<State>();
+      await builder.Build().RunAsync();
+    }
+  }
+}
