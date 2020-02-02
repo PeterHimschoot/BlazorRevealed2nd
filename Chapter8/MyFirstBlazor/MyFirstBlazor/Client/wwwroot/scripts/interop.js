f478430ad533fa8a598a3e85e06a4a89ee7ed71c
Peter Himschoot <peter@u2u.be>
2020-02-02 18:42:13 +0100

    C8

---
+++ Chapter8/MyFirstBlazor/MyFirstBlazor/Client/wwwroot/scripts/interop.js
@@ -0,0 +1,18 @@
+﻿(function () {
+  window.blazorLocalStorage = {
+    get: key => key in localStorage ? JSON.parse(localStorage[key]) : null,
+    set: (key, value) => { localStorage[key] = JSON.stringify(value); },
+    delete: key => { delete localStorage[key]; },
+    watch: async (instance) => {
+
+      window.addEventListener('storage', (e) => {
+        console.log("update");
+        instance.invokeMethodAsync('UpdateCounter');
+      });
+
+      let time = await DotNet.invokeMethodAsync("Blazor.JSInterop.Client", "GetLocalTime");
+      console.log('registering for storage events at ' + time);
+
+    }
+  };
+})();
