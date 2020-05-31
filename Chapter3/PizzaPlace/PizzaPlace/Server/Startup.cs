58b2094a15412f69559e5172b882292f1f6ea3fa
Peter Himschoot <peter@u2u.be>
2020-05-31 11:10:01 +0200

    Update to RTM

--- Chapter3/PizzaPlace/PizzaPlace/Server/Startup.cs
+++ Chapter3/PizzaPlace/PizzaPlace/Server/Startup.cs
@@ -12,7 +12,7 @@ namespace PizzaPlace.Server
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMvc();
+      services.AddControllers();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
